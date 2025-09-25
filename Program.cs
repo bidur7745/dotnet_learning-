8c9ecee175868b7d39464b373a788c310d967d83
bidur7745 <bidursiwakoti2062@gmail.com>
2025-09-25 23:02:13 +0545

    learning variables and operator in c sharp

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int a = 12;
+            int b= 13;
+            int c= 14;
+            Console.WriteLine("Hello World");
+            Console.WriteLine($"The sum of a, b and c is {a+b+c}");
+            if( (a > b) && (a > c))
+            {
+                Console.WriteLine("a is the highest");
+            }
+            else if((b > a) && (b > c))
+            {
+                Console.WriteLine("b is the highest");
+            }
+            else
+            {
+                Console.WriteLine("c is the highest");
+             }
+                Console.ReadLine();
+        }
+    }
+}
